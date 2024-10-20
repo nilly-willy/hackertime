dd33f50421661e5cf0e59e168492d1a5d5ff8451
Kritika Pantha <kpantha@berkeley.edu>
2024-10-20 02:21:03 -0700

    setup nav menu, need to fix dropdown

--- recipe-app/src/components/navbar.js
+++ recipe-app/src/components/navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import "../Navbar.css";
+import '../Navbar.css'; 
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,16 +10,24 @@ function Navbar() {
 
   return (
     <nav className="navbar">
+      {/* Logo Section */}
+      <div className="logo">
+        <img src={require('../assets/remi1.png')} alt="Logo" /> {/* Adjust path to logo */}
+      </div>
+
+      {/* Hamburger Menu Icon */}
       <div className="hamburger" onClick={toggleMenu}>
         <span className="line"></span>
         <span className="line"></span>
         <span className="line"></span>
       </div>
 
+      {/* Navigation Menu */}
       <ul className={`menu ${isOpen ? 'open' : ''}`}>
         <li><a href="#home">Home</a></li>
-        <li><a href="#recipes">Recipes</a></li>
         <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
   );
